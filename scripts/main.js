d2b7ab93c242f5d2d3a34be07b5d973ecda578e2
nacho <nacholeon30@hotmail.com>
2021-07-16 14:47:40 -0300

    add button to show/hide book input form

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+const bookInput = document.querySelector('#book_input');
+const bookList = document.querySelector('#book_list');
 let library = [];
 
 function Book(title, author, pages, read) {
@@ -18,6 +20,26 @@ function addBookToLibrary() {
         document.querySelector('#pages_input').value,
         document.querySelector('#read_input').checked
     ) );
+    displayBook();
+}
+
+function displayBook() {
+    const bookRow = document.createElement('tr');
+    const bookTitleCell = document.createElement('td');
+    bookTitleCell.textContent = library[library.length-1].title;
+    bookRow.appendChild(bookTitleCell);
+    const bookAuthorCell = document.createElement('td');
+    bookAuthorCell.textContent = library[library.length-1].author;
+    bookRow.appendChild(bookAuthorCell);
+    const bookPagesCell = document.createElement('td');
+    bookPagesCell.textContent = library[library.length-1].pages;
+    bookRow.appendChild(bookPagesCell);
+    const bookReadCell = document.createElement('td');
+    bookReadCell.textContent = (library[library.length-1].read)?'yes':'no';
+    bookRow.appendChild(bookReadCell);
+    bookList.appendChild(bookRow);
 }
 
 document.querySelector('#add_book').addEventListener('click', addBookToLibrary);
+
+document.querySelector('#new_book').addEventListener('click', () => bookInput.classList.toggle("hidden"));
