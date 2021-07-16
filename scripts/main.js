da55c1c04bad257de43905a65048aa26a2439ebf
nacho <nacholeon30@hotmail.com>
2021-07-16 12:50:42 -0300

    add books input

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
+let library = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
     this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${(read)?'already read':'not read yet'}`;
+        return `"${title}" by ${author}, ${pages} pages, ${(read)?'already read':'not read yet'}`;
     }
     console.log(this.info());
 }
+
+function addBookToLibrary() {
+    library.push( new Book(
+        document.querySelector('#title_input').value,
+        document.querySelector('#author_input').value,
+        document.querySelector('#pages_input').value,
+        document.querySelector('#read_input').checked
+    ) );
+}
+
+document.querySelector('#add_book').addEventListener('click', addBookToLibrary);
