2de5c0d5e3115eb6341b4f515de956aea32eec2c
nacho <nacholeon30@hotmail.com>
2021-07-28 18:05:03 -0300

    change object constructor to class

--- scripts/main.js
+++ scripts/main.js
@@ -7,18 +7,19 @@ let library = [];
 let libraryRow = [];
 let bookColors = [];
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-}
-
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${(this.read)?'already read':'not read yet'}`;
-}
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    info() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${(this.read)?'already read':'not read yet'}`;
+    }
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
 loadData();
